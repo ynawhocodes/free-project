029d4a995ea28473fecfa6e2375d0cf63efb6406
ynawhocodes <ynawhocodes@gmail.com>
2023-10-11 15:31:26 +0900

    [feat] add BlurAndScaleOnScroll component

---
+++ src/app/(components)/BlurAndScaleOnScroll.tsx
@@ -0,0 +1,59 @@
+import React, { useRef, useEffect } from "react";
+import { gsap, Power0 } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { Star } from "../_assets/icons";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const BlurAndScaleOnScroll: React.FC = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    ScrollTrigger.create({
+      trigger: container,
+      start: "top 1%",
+      end: "bottom 0%",
+      onEnter: () => {
+        gsap.to(container, {
+          duration: 0.5,
+          scale: 1,
+          ease: Power0.easeNone,
+          onComplete: () => {
+            gsap.to(container, {
+              duration: 0.5,
+              filter: "blur(5px)",
+              ease: Power0.easeNone,
+            });
+          },
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(container, {
+          duration: 3.5,
+          scale: 0.2,
+          filter: "blur(0px)",
+          ease: Power0.easeNone,
+        });
+      },
+    });
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      className=""
+      style={{
+        transition: "filter 0.5s, transform 0.5s",
+        display: "inline-block",
+        transformOrigin: "center center",
+        transform: "scale(0.2)",
+      }}
+    >
+      <Star />
+    </div>
+  );
+};
+
+export default BlurAndScaleOnScroll;
