cdb828b497ae401a2c35bb3fa2cc8fd364e17237
ynawhocodes <ynawhocodes@gmail.com>
2023-10-12 00:45:46 +0900

    [feat] add text animation in RedLabel

--- src/app/(components)/RedLabel.tsx
+++ src/app/(components)/RedLabel.tsx
@@ -4,41 +4,41 @@ const RedLabel = () => {
       <div className="animate-InfinityRolling">
         <div className="flex items-center">
           <div className="flex items-center">
-            <p className="title-lg">
+            <p className="title-lg text-animation-gradient">
               (&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;
             </p>
             <div>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
             </div>
-            <p className="title-lg">
+            <p className="title-lg text-animation-gradient">
               &nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;)
             </p>
           </div>
           <div className="flex items-center">
-            <p className="title-lg">
+            <p className="title-lg text-animation-gradient">
               (&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;
             </p>
             <div>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
             </div>
-            <p className="title-lg">
+            <p className="title-lg text-animation-gradient">
               &nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;)
             </p>
           </div>
           <div className="flex items-center">
-            <p className="title-lg">
+            <p className="title-lg text-animation-gradient">
               (&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;
             </p>
             <div>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
-              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
+              <p className="h-[20px] content-md w-[120px] text-animation">you never know</p>
             </div>
-            <p className="title-lg">
+            <p className="title-lg text-animation-gradient">
               &nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;)
             </p>
           </div>
