8fce77d47087322b8e3e3ea58f481872a5015cc4
ynawhocodes <ynawhocodes@gmail.com>
2023-10-11 15:35:06 +0900

    [feat] add RedLabel component

---
+++ src/app/(components)/RedLabel.tsx
@@ -0,0 +1,51 @@
+const RedLabel = () => {
+  return (
+    <div className="bg-[#FF0000] w-full overflow-x-hidden">
+      <div className="animate-InfinityRolling">
+        <div className="flex items-center">
+          <div className="flex items-center">
+            <p className="title-lg">
+              (&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;
+            </p>
+            <div>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+            </div>
+            <p className="title-lg">
+              &nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;)
+            </p>
+          </div>
+          <div className="flex items-center">
+            <p className="title-lg">
+              (&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;
+            </p>
+            <div>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+            </div>
+            <p className="title-lg">
+              &nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;)
+            </p>
+          </div>
+          <div className="flex items-center">
+            <p className="title-lg">
+              (&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;((&nbsp;&nbsp;(&nbsp;&nbsp;
+            </p>
+            <div>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+              <p className="h-[20px] content-md w-[120px]">you never know</p>
+            </div>
+            <p className="title-lg">
+              &nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;))&nbsp;&nbsp;)&nbsp;&nbsp;)
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RedLabel;
