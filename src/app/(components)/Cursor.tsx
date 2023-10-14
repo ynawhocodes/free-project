7afb4c3ecf8b7dc34ef22f4dad5eb53304d83609
ynawhocodes <ynawhocodes@gmail.com>
2023-10-14 17:17:09 +0900

    [mod] modify cursor effect

--- src/app/(components)/Cursor.tsx
+++ src/app/(components)/Cursor.tsx
@@ -20,8 +20,23 @@ const Cursor = () => {
           top: `${y - 50}px`,
         }}
       ></div> */}
+      {/* {Array(6)
+        .fill(0)
+        .map((el, i) => {
+          return (
+            <div
+              className="ease-linear duration-100 absolute inset-0 translate-x-0 group-hover:translate-y-[00px]"
+              style={{
+                transitionDelay: `${i * 100}ms`,
+              }}
+              key={i}
+            >
+              <PinkStar />
+            </div>
+          );
+        })} */}
       <div
-        className={`fixed w-[140px] z-50 pointer-events-none transition-transform duration-200 ease-in-out mix-blend-difference ${
+        className={`fixed w-[140px] z-50 pointer-events-none transition-transform duration-200 ease-in-out ${
           isClicked && "scale-150"
         }`}
         style={{
