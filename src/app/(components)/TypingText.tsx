9123b0fbcefd9c61bbcc3a553c40dc4622515652
ynawhocodes <ynawhocodes@gmail.com>
2023-10-14 17:18:11 +0900

    [mod] modify TypingText

--- src/app/(components)/TypingText.tsx
+++ src/app/(components)/TypingText.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect } from "react";
 import gsap from "gsap";
 import { TextPlugin } from "gsap/dist/TextPlugin";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
@@ -8,31 +6,32 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(TextPlugin);
 gsap.registerPlugin(ScrollTrigger);
 
-const TypingText = ({from, to}: {from: string; to: string;}) => {
+const TypingText = ({ from, to }: { from: string; to: string }) => {
   const textRef = useRef(null);
+
 
   useEffect(() => {
     const textElement = textRef.current;
 
-    gsap.to(textElement, 
-      {
+    gsap.to(textElement, {
       text: `${to}`,
       duration: 2,
       delay: 1,
       scrollTrigger: {
         trigger: textElement,
-        start: "center center",
+        start: "80% center",
         end: "center top",
         scrub: true,
-        // markers: true,
       },
     });
   }, []);
 
   return (
-    <p className="sticky top-0 title-lg py-[10%] mix-blend-difference" ref={textRef}>
-      {from}
-    </p>
+    <div className="sticky top-0 pt-[85%] pb-[10%] text-center h-fit">
+      <p className="title-lg" ref={textRef}>
+        {from}
+      </p>
+    </div>
   );
 };
 
