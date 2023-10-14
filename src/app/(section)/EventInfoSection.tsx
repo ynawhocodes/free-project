714fec3fc1812a2172538abdd4a5f892fc7e8059
ynawhocodes <ynawhocodes@gmail.com>
2023-10-15 02:20:21 +0900

    [mod] modify event info contents

--- src/app/(section)/EventInfoSection.tsx
+++ src/app/(section)/EventInfoSection.tsx
@@ -32,17 +32,17 @@ const EventInfoSection = () => {
             <br />
             선발방식 및 기간 | 추첨&apos; ~2023/10/20 23:59
             <br />
-            참여조건 | <a className="underline underline-offset-[3px] decoration-2 hover:text-white" href="https://www.instagram.com/01.works">영원 인스타그램(@01.works) 팔로우</a>&apos; 해당 게시글
+            참여조건 | <a className="underline underline-offset-[4px] decoration-1 hover:text-white" href="https://www.instagram.com/01.works">영원 인스타그램(@01.works) 팔로우</a>&apos; 해당 게시글
             스토리로 공유
             <br />
             참여방법 | “[이벤트 지원] - ❍❍❍(이름)”를 제목으로
-            첨부파일(자유양식)과 함께 <a className="underline underline-offset-[3px] decoration-2 hover:text-white" href={"mailto:01.works"}>contact@01.work</a>로 이메일 발송
+            첨부파일(자유양식)과 함께 <a className="underline underline-offset-[4px] decoration-1 hover:text-white" href={"mailto:01.works"}>contact@01.work</a>로 이메일 발송
             <br />
             첨부파일 필수내용 | 화면설계서 또는 디자인 산출물(figma)&apos;
-            자세한 내용과 예시는 <a className="underline underline-offset-[3px] decoration-2 hover:text-white" href="https://free.01.works">https://free.01.works</a> 참고
+            자세한 내용과 예시는 <a className="underline underline-offset-[4px] decoration-1 hover:text-white" href="https://free.01.works">https://free.01.works</a> 참고
             <br />
             발표 | 2023/10/21 개인 메일로 전달
-            <br />
+            <br /> 
             <br />
             <span className="font-bold">❍ 개발 방식</span>
             <br />
@@ -61,6 +61,8 @@ const EventInfoSection = () => {
             웹 사이트 배포를 원할 경우 도메인과 호스팅 비용은 개인 부담입니다.
             <br />
             (사용 기한&apos; 주인 명시)
+            <br/>
+            디자인에 따라 일정이 변경될 수도 있습니다.
             <br />
             <br />
             자세한 문의는 contact@01.works 또는 Instagram DM으로 부탁드립니다.
