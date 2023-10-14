49b917ecb5ee8877d6cc43956814f0d46f86aa1e
ynawhocodes <ynawhocodes@gmail.com>
2023-10-14 17:19:24 +0900

    [mod] modify blackbox effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,12 @@ import RedLabel from "./(components)/RedLabel";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import ChangeTextColor from "./(components)/ChangeTextColor";
-import EventInfo from "./(components)/EventInfo";
 import TypingText from "./(components)/TypingText";
 import Cursor from "./(components)/Cursor";
 import ExamplePage from "./_assets/example";
+import ExampleSection from "./(section)/ExampleSection";
+import EventInfoSection from "./(section)/EventInfoSection";
+import { useScroll } from "./_hooks/useScroll";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -21,6 +23,8 @@ export default function Home() {
   const textRef2 = useRef(null);
   const textRef3 = useRef(null);
   const divRef = useRef(null);
+  const topBoxRef = useRef(null);
+  const bottomBoxRef = useRef(null);
 
   useEffect(() => {
     const sharpElement = sharp.current;
@@ -28,11 +32,23 @@ export default function Home() {
     const textElement2 = textRef2.current;
     const textElement3 = textRef3.current;
     const divElement = divRef.current;
+    const topBoxElement = topBoxRef.current;
+    const bottomBoxElement = bottomBoxRef.current;
 
     gsap.set(sharpElement, { opacity: 0, x: "-100%" });
     gsap.set(textElement1, { opacity: 0, x: "-100%" });
     gsap.set(textElement2, { opacity: 0, x: "-100%" });
     gsap.set(textElement3, { opacity: 0, x: "-100%" });
+
+    gsap.to(topBoxElement, {
+      height: 0,
+      scrollTrigger: {
+        trigger: topBoxElement,
+        start: "80% center",
+        end: "center top",
+        scrub: true,
+      },
+    });
 
     gsap.to(sharpElement, {
       opacity: 1,
@@ -52,8 +68,8 @@ export default function Home() {
       ease: "power4.out",
       scrollTrigger: {
         trigger: textElement1,
-        start: `top 60%`,
-        end: "bottom 20%",
+        start: `top 70%`,
+        end: "bottom center",
         scrub: true,
       },
     });
@@ -63,8 +79,8 @@ export default function Home() {
       ease: "power4.out",
       scrollTrigger: {
         trigger: textElement2,
-        start: `top center`,
-        end: "bottom 23%",
+        start: `top 65%r`,
+        end: "bottom center",
         scrub: true,
       },
     });
@@ -74,8 +90,8 @@ export default function Home() {
       ease: "power4.out",
       scrollTrigger: {
         trigger: textElement3,
-        start: `top 67%`,
-        end: "bottom 27%",
+        start: `top 60%`,
+        end: "bottom center",
         scrub: true,
       },
     });
@@ -83,70 +99,91 @@ export default function Home() {
     gsap.to(divElement, {
       scrollTrigger: {
         trigger: divElement,
-        start: "top 80%",
-        end: "bottom center",
+        start: "top 60%",
+        end: "top center",
         scrub: true,
         // markers: true,
       },
-      rotation: 360,
+      rotation: -45,
       duration: 1,
     });
   }, []);
+
+  const { scrollY } = useScroll();
+  const height = window.innerHeight;
+
+  console.log(scrollY, height, ((height - scrollY) / height) * 100);
 
   return (
     <main className="h-full">
       <div className="fixed left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
         <BlurAndScaleOnScroll />
       </div>
-      <div className="h-full">
-        <div className="relative h-[100vh]">
-          <div className="flex justify-center items-center h-full sticky top-0">
-            <TypingText to="Spread your wide wing" from="SPREAD YOUR WIDE WING" />
-          </div>
-        </div>
-        <div className="relative">
-          <div className="sticky top-0 h-fit">
-            <div className="flex justify-center" ref={sharp}>
-              <Sharp />
-            </div>
-            <div className="flex justify-center pt-[10%]" ref={textRef1}>
-              <p className="title-md">Maybe</p>
-            </div>
-            <div className="flex justify-center pb-[10%]" ref={textRef2}>
-              <p className="title-md">something great will come out</p>
-            </div>
+      <div className="relative h-[200vh]">
+        {/* <div className="bg-black/50 w-full h-[20%] sticky top-0 z-50"></div> */}
+        {/* <div className="bg-black/50 w-full h-[20%] sticky bottom-[50%] z-50"></div> */}
+        <div className="flex justify-center h-full sticky top-0">
+          {/* <div
+            className="w-full bg-black absolute"
+            style={{ height: "50%" }}
+            ref={topBoxRef}
+          ></div> */}
+          <div
+            className={`fixed inset-0 flex flex-col justify-evenly`}
+            // ref={bottomBoxRef}
+          >
             <div
-              className="flex justify-center pb-[20%] mt-[60%]"
-              ref={textRef3}
-            >
-              <p className="title-md">&lsquo;Cause you&rsquo;re a genius</p>
-            </div>
-            <RedLabel />
-          </div>
-        </div>
-        {/* <div className="flex justify-center items-center relative h-full"> */}
-        <ChangeTextColor text="(Mail) contact@01.works" />
-        {/* </div> */}
-        <EventInfo />
-        <div className="h-full py-10 relative">
-          <div className="flex sticky top-0">
-            <div className="pt-10 pl-1">
-              <div className="border-1">
-              <Example />
-              </div>
-            </div>
-            <div className="flex-1">
-              <ExamplePage/>
-            </div>
-          </div>
-        </div>
-        <div className="h-full bg-black flex justify-center items-center z-20 relative">
-          <div className="flex justify-center w-full" ref={divRef}>
-            <Sticker />
+              className="bg-black w-full"
+              style={{
+                flexBasis: `${((height - scrollY) / height) * 100}%`,
+              }}
+            ></div>
+            <TypingText
+              from="SPREAD YOUR WIDE WING"
+              to="Spread your wide wing"
+            />
+            <div
+              className="bg-black w-full"
+              style={{
+                flexBasis: `${((height - scrollY) / height) * 100}%`,
+              }}
+            ></div>
           </div>
         </div>
       </div>
-      <Cursor/>
+      <div className="relative">
+        <div className="sticky top-0 h-fit">
+          <div className="flex justify-center absolute w-full" ref={sharp}>
+            {/* {Array(6).fill(0).map((el,i) => {
+                return <div className="ease-linear duration-100 absolute inset-0 translate-x-0 group-hover:translate-y-[00px]" style={{
+                  transitionDelay: `${i*100}ms`
+                }} key={i} ref={sharp}><Sharp /></div>
+              })} */}
+            <Sharp />
+          </div>
+          <div className="flex justify-center pt-[10%]" ref={textRef1}>
+            <p className="title-md">Maybe</p>
+          </div>
+          <div className="flex justify-center pb-[10%]" ref={textRef2}>
+            <p className="title-md">something great will come out</p>
+          </div>
+          <div className="flex justify-center pb-[20%] mt-[60%]" ref={textRef3}>
+            <p className="title-md">&lsquo;Cause you&rsquo;re a genius</p>
+          </div>
+          <RedLabel />
+        </div>
+      </div>
+      {/* <div className="flex justify-center items-center relative h-full"> */}
+      <ChangeTextColor text="(Mail) contact@01.works" />
+      {/* </div> */}
+      <EventInfoSection />
+      <ExampleSection />
+      <div className="h-full bg-black flex justify-center items-center z-20 relative">
+        <div className="flex justify-center w-full" ref={divRef}>
+          <Sticker />
+        </div>
+      </div>
+      <Cursor />
     </main>
   );
 }
